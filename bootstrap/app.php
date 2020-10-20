937ded4ebb9b4d5e6a1b596dbbd07923b0de7e15
Vyan-LaptopAsus <vyanaryprabowo9763@gmail.com>
2020-10-21 01:58:07 +0700

    add generator lumen

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -23,9 +23,10 @@ $app = new Laravel\Lumen\Application(
     dirname(__DIR__)
 );
 
-// $app->withFacades();
+$app->withFacades();
 
-// $app->withEloquent();
+$app->withEloquent();
+
 
 /*
 |--------------------------------------------------------------------------
@@ -94,6 +95,8 @@ $app->configure('app');
 // $app->register(App\Providers\AppServiceProvider::class);
 // $app->register(App\Providers\AuthServiceProvider::class);
 // $app->register(App\Providers\EventServiceProvider::class);
+
+$app->register(Flipbox\LumenGenerator\LumenGeneratorServiceProvider::class);
 
 /*
 |--------------------------------------------------------------------------
